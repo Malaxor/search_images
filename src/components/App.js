5eb5f1fbd3d59885a75c05b88d0c0ea2c23b5330
PHILIP COSTACHE <nogobaduk@gmail.com>
2020-05-26 12:56:41 -0400

    axios install, import and setup for unsplash API

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,17 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 import SearchBar from './SearchBar';
 
 class App extends Component {
    onSearchSubmit(term) {
-      console.log(term);
+      axios.get('https://api.unsplash.com/search/photos', {
+         params: { 
+            query: term 
+         },
+         headers: { 
+            Authorization: `Client-ID 4rck814Vk6fHTTlK7SZExJRzKSqZZYVJV2ZY665z_Eg` 
+         }
+      });
    }
    render() {
       return (
